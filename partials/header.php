3fe3a44552442483257033616f5615deec07287f
cherenkevich <cherenkevich.com@gmail.com>
2014-09-03 00:29:39 +0300

    Решение проблемы с поиском, когда блог установлен в поддиректорию

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
     <header class="header">
       <div class="header__logo">
-        <a href="/" title="<?php echo __( 'To home page', 'zero' ) ?>" class="logo"><?php echo get_bloginfo( 'name' ) ?></a>
+        <a href="<?php echo get_bloginfo('url') ?>/" title="<?php echo __( 'To home page', 'zero' ) ?>" class="logo"><?php echo get_bloginfo( 'name' ) ?></a>
       </div>
       <div class="header__nav">
         <?php
@@ -12,7 +12,7 @@
         ?>
       </div>
       <div class="header__search">
-        <form action="/" method="get" name="search" class="search">
+        <form action="<?php echo get_bloginfo('url') ?>/" method="get" name="search" class="search">
           <label for="s" class="search__label"><?php echo __( 'Search', 'zero' ) ?></label>
           <input type="text" name="s" id="s" class="search__field" value="<?php echo ( isset( $_GET['s'] ) && $_GET['s'] ? $_GET['s'] : '' ) ?>">
         </form>
